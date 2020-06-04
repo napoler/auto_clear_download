63314e95a5b98b7f2d7b31776848bbba63c5333c
Terry chan <napoler2008@gmail.com>
2020-06-04 23:23:29 +0800

    Signed-off-by: Terry chan <napoler2008@gmail.com>

--- run.py
+++ run.py
@@ -2,22 +2,31 @@
 import os
 import timeit
 # rlist=["家里"]
-path=input("输入目录：")
-f2 = open("remove.txt","r")
-rlist = f2.readlines()
+
 def main():
-    for pathname,dirnames,filenames in os.walk(path):
+    rpath=raw_input("输入目录：")
+    #rpath="/mnt/c/transmission/aria"
+    f2 = open("remove.txt","r")
+    rlist = f2.readlines()
+    rn=[]
+    for pathname,dirnames,filenames in os.walk(rpath):
         for filename in filenames:
             file=os.path.join(pathname,filename)
             print(file)
             for it in rlist:
-                if it in  file:
+                it=it.replace("\n", "")
+                #print(it,file)
+                #print(str(it) in  str(file))
+                if it in  str(file):
                     print("删除")
+                    rn.append(file)
                     os.remove(file)
                     break
                 else:
-                    # print("忽略")
+                    #print("忽略")
                     pass
             # os.remove(file)
+    print("删除：",rn)
+    print("共计删除：",len(rn))
 if __name__=='__main__':
     main()
